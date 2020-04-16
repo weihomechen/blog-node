eb2fbf6fea45347862515ff86468b5cd3e15214d
moyu <weihome_chen@163.com>
2020-04-16 15:39:21 +0800

    chore(gitignore): make git ignore work

--- config/config.private.js
+++ config/config.private.js
@@ -1,11 +1,11 @@
 // 此文件因为开源具有一定风险，第一次提交后不再纳入git管理，参考 git update-index --assume-unchanged
 // 相关敏感信息请自行获取
 exports.ossConfig = {
-  accessKeyId: 'youraccessKeyId',
-  accessKeySecret: 'youraccessKeySecret',
+  accessKeyId: 'yourAccessKeyId',
+  accessKeySecret: 'yourAccessKeySecret',
 };
 
 exports.alinodeConfig = {
-  appid: 'yourappid',
-  secret: 'yoursecret',
+  appid: 'yourAppId',
+  secret: 'yourSecret',
 };
